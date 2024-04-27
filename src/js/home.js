e26b2f5aa5889686132e7eb0118c77f4840cc447
Gary <kululu.gary@gmail.com>
2024-04-27 02:36:16 +0200

    Updated: 2024-04-27 02:36:15

--- src/js/home.js
+++ src/js/home.js
@@ -1,28 +1,4 @@
 document.onreadystatechange = () => {
   if (document.readyState == "complete") {
-    gameOfTheDay();
   }
 };
-
-async function gameOfTheDay() {
-  const seed = new Date().getDay() + new Date().getMonth();
-
-  const gameOfTheDayContainer = document.querySelector("#game-of-the-day");
-  const imageElement = gameOfTheDayContainer.querySelector("img");
-  const textElement = gameOfTheDayContainer.querySelector("div");
-
-  const data = await fetch("/assets/data/games.json").then((res) => res.json());
-  const selectedGame = data.completed[seed % data.completed.length];
-
-  imageElement.src = selectedGame.image;
-
-  const linkElement = document.createElement("a");
-  linkElement.href = selectedGame.link;
-  linkElement.innerText = selectedGame.title;
-
-  const authorElement = document.createElement("p");
-  authorElement.innerText = `- ${selectedGame.platform}`;
-
-  textElement.appendChild(linkElement);
-  textElement.appendChild(authorElement);
-}
---
+++ src/js/settings.js
@@ -0,0 +1,37 @@
+document.onreadystatechange = () => {
+  if (document.readyState == "complete") {
+    setUpSettings();
+    setUpThemeSwitcher();
+  }
+};
+
+function setUpSettings() {
+  const theme = localStorage.getItem("theme");
+
+  if (theme) {
+    if (theme === "theme-dark") {
+      document.documentElement.classList.add("darkmode");
+    } else {
+      document.documentElement.classList.remove("darkmode");
+    }
+  } else if (window.matchMedia) {
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (prefersDarkMode) {
+      document.documentElement.className = "darkmode";
+    }
+  }
+}
+
+function setUpThemeSwitcher() {
+  const themeSwitcher = document.getElementById("theme-switch");
+
+  themeSwitcher.addEventListener("change", () => {
+    document.documentElement.classList.toggle("darkmode");
+
+    localStorage.setItem(
+      "theme",
+      document.documentElement.classList.contains("darkmode") ? "theme-dark" : "theme-light"
+    );
+  });
+}
