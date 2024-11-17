2e1871f55fa58fbf1379b3b01d1be3db8867711f
Gary <kululu.gary@gmail.com>
2024-11-17 21:29:00 +0100

    Updated: 2024-11-17 21:29:00

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,24 +4,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const themePicker = document.querySelector("#theme-select");
   const fontPicker = document.querySelector("#font-select");
+  const resetButton = document.querySelector("#reset-button");
 
-  if (cachedTheme) themePicker.value = cachedTheme;
+  setDefaultTheme();
   if (cachedFontStack) fontPicker.value = cachedFontStack;
 
-  themePicker.addEventListener("change", handleChangeTheme);
-  fontPicker.addEventListener("change", handleChangeFontStack);
+  resetButton.addEventListener("click", handleResetForm);
+  themePicker.addEventListener("change", (e) =>
+    handleChangeTheme(e.target.value)
+  );
+  fontPicker.addEventListener("change", (e) =>
+    handleChangeFontStack(e.target.value)
+  );
 
-  function handleChangeTheme(e) {
-    const newTheme = e.target.value;
+  function setDefaultTheme() {
+    if (cachedTheme) {
+      themePicker.value = cachedTheme;
+    } else {
+      if (window.matchMedia("(prefers-color-scheme: dark)")) {
+        themePicker.value = "dark-blue";
+      } else {
+        themePicker.value = "grayscale";
+      }
+    }
+  }
 
+  function handleChangeTheme(newTheme) {
     localStorage.setItem("theme", newTheme);
     document.documentElement.setAttribute("data-theme", newTheme);
   }
 
-  function handleChangeFontStack(e) {
-    const newFont = e.target.value;
-
+  function handleChangeFontStack(newFont) {
     localStorage.setItem("font-stack", newFont);
     document.documentElement.setAttribute("data-font-stack", newFont);
+  }
+
+  function handleResetForm(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("AAA");
+    localStorage.removeItem("theme");
+    localStorage.removeItem("font-stack");
+
+    window.location.reload(true);
   }
 });
