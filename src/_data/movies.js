810dd9d30b16955e62d4833a08c193e4f453f3b7
Gary <kululu.gary@gmail.com>
2025-09-06 23:50:23 +0200

    Updated: 2025-09-06 23:50:22

--- src/_data/movies.js
+++ src/_data/movies.js
@@ -127,6 +127,8 @@ async function getCollection() {
     args: [
       "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
       "--disable-features=site-per-process",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
     ],
   });
 
