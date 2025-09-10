0f56b14b12245d610eeb5383abcee8504b08fe59
Gary <kululu.gary@gmail.com>
2025-09-10 18:56:11 +0200

    Updated: 2025-09-10 18:56:11

--- src/js/utils/save.js
+++ src/js/utils/save.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 function saveTestData(fileName, data) {
+  if (process.env.ENVIRONMENT !== "DEVELOPMENT") return;
   fs.writeFileSync(`${__dirname}/_test/${fileName}`, JSON.stringify(data));
 }
 
